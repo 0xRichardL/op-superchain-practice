f589234cd2ccfdd78f7119a204a80ada8e84847a
Richard <thanhle@ancient8.gg>
2025-03-31 16:23:44 +0700

    chore: redeploy contract fix: gas limit error

--- scripts/interop_greeting.ts
+++ scripts/interop_greeting.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { createPublicClient, createWalletClient, http } from 'viem';
+import { createPublicClient, createWalletClient, http, publicActions } from 'viem';
 import GreeterAbi from './abi/greeter.abi';
 import GreetingSenderAbi from './abi/greeting-sender.abi';
 import { delay } from './utils/time.util';
@@ -7,22 +7,24 @@ import { A, B } from './constants/chains';
 import { account_0 } from './constants/accounts';
 
 async function main() {
-  const clientA = createWalletClient({ ...A, account: account_0 });
+  const clientA = createWalletClient({ ...A, account: account_0 }).extend(publicActions);
   const clientB = createPublicClient(B);
 
   const hash = await clientA.writeContract({
     abi: GreetingSenderAbi,
-    address: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+    address: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
     functionName: 'setGreeting',
     args: ['Hello from A'],
+    gasPrice: await clientA.getGasPrice(),
+    gas: 100000n,
   });
   console.log('Hash:', hash);
-
-  await delay(2000);
+  await delay(4000);
   const result = await clientB.readContract({
     abi: GreeterAbi,
     address: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
     functionName: 'greet',
+    args: [],
   });
   console.log('Result:', result);
 }
